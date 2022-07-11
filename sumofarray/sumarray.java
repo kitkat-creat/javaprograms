58a51d938e3146e61dc63225e81c3d85a714d3b7
KathyA <100660595+kitkat-creat@users.noreply.github.com>
2022-07-11 14:03:44 -0700

    Add files via upload

---
+++ sumofarray/sumarray.java
@@ -0,0 +1,25 @@
+public class sumarray {
+    private int[] nums;
+
+    public sumarray() {
+        nums = new int[5];
+    }
+
+    public void setElem(int index,int value) {
+
+            nums[index] = value;
+            //inserting input into num index using value
+
+
+
+    }
+
+    public int getelem(int i) {
+
+
+
+        return nums[i];
+
+    }
+
+}
